c5c26b73693c56b31fc02001cd114901a7761c8e
asafedainez <asafe.sousa@gmail.com>
2022-04-30 16:05:56 -0300

    feat(página de configuração): implementada página de configuração com sua estilização

---
+++ src/pages/config/Config.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Header from '../../components/Header';
+
+class Config extends Component {
+  render() {
+    return (
+      <section>
+        <Header />
+        <h1 data-testid="settings-title">Configuração</h1>
+      </section>
+    );
+  }
+}
+
+export default connect()(Config);

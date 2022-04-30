48373227b5f288baad8322a834c53b6fc6668b54
asafedainez <asafe.sousa@gmail.com>
2022-04-30 14:39:02 -0300

    feat(redux): implementando actions e action types

---
+++ src/redux/actions/actionTypes.action.js
@@ -0,0 +1,7 @@
+export const ADD_PLAYER = 'ADD_PLAYER';
+export const ADD_ASSERTION = 'ADD_ASSERTION';
+export const ADD_SCORE = 'ADD_SCORE';
+export const ADD_TOKEN = 'ADD_TOKEN';
+export const ADD_QUESTIONS = 'ADD_QUESTIONS';
+export const ERROR_API = 'ERROR_API';
+export const RECOVERY_TOKEN = 'RECOVERY_TOKEN';

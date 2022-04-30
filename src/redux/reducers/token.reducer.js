dedd4032d6aabb88c52814409d9301020d6d8f09
asafedainez <asafe.sousa@gmail.com>
2022-04-30 14:48:46 -0300

    feat(reducer redux): implementando reducer do token

---
+++ src/redux/reducers/token.reducer.js
@@ -0,0 +1,18 @@
+import { ADD_TOKEN, ERROR_API, RECOVERY_TOKEN } from '../actions/actionsType';
+
+const INITIAL_STATE = '';
+
+const token = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case ADD_TOKEN:
+    return action.payload.token;
+  case ERROR_API:
+    return { ...state, error: action.payload };
+  case RECOVERY_TOKEN:
+    return action.payload;
+  default:
+    return state;
+  }
+};
+
+export default token;

c36aabbc8b0941f780ff7dfa56dd8345644b16f9
asafedainez <asafe.sousa@gmail.com>
2022-04-30 14:49:28 -0300

    feat(reducer redux): implementando combine reducer em todos os reducers

---
+++ src/redux/reducers/reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import player from './player';
+import questions from './questions';
+import token from './token';
+
+export default combineReducers({ player, questions, token });
